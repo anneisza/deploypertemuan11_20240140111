cf101fc8074d1170e4996dc78f2b9021328ba40a
anneisza <rizaanneisza@gmail.com>
2026-05-19 19:37:24 +0700

    tambah repository untuk entitas Profile

--- src/main/java/com/deploy/pertemuan11/repository/ProfileRepositoy.java
+++ src/main/java/com/deploy/pertemuan11/repository/ProfileRepositoy.java
@@ -1,4 +1,9 @@
 package com.deploy.pertemuan11.repository;
 
-public interface ProfileRepositoy {
+import com.deploy.pertemuan11.model.Profile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProfileRepositoy extends JpaRepository<Profile, String> {
 }
